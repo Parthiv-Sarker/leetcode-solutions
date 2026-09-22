bfa570ade44467134fe3d1e0f588ea3c7bd55e41
Parthiv Sarker <126268558+Parthiv-Sarker@users.noreply.github.com>
2026-09-22 11:42:38 +0530

    Add Find X Value of Array II solution

---
+++ Java/3840 - Find-X-Value-of-Array-II/README.md
@@ -0,0 +1,36 @@
+# Find X Value of Array II
+
+## Problem
+
+- **Problem ID:** 3840
+- **Difficulty:** Hard
+- **Language:** Java
+
+## Topics
+
+- Array
+- Math
+- Segment Tree
+
+## LeetCode
+
+- **Submission ID:** 2149392536
+- **Runtime:** 203 ms
+- **Runtime Percentile:** 89.47%
+- **Memory:** 286.1 MB
+- **Memory Percentile:** 10.53%
+
+[View Submission](https://leetcode.com/problems/find-x-value-of-array-ii/submissions/2149392536/?envType=daily-question&envId=2026-09-22)
+
+## Solution
+
+The solution is available in [solution.java](./solution.java).
+
+## Complexity
+
+- **Time Complexity:** Not provided by LeetCode
+- **Space Complexity:** Not provided by LeetCode
+
+---
+
+Generated automatically by LeetCode GitHub Sync.
---
+++ Java/3840 - Find-X-Value-of-Array-II/solution.java
@@ -0,0 +1,131 @@
+class Solution {
+
+    static class Node {
+        int prod;
+        int[] cnt;
+
+        Node(int k) {
+            cnt = new int[k];
+            prod = 1 % k;
+        }
+    }
+
+    int n, k;
+    Node[] tree;
+    int[] nums;
+
+    // Merge two adjacent segments: left + right
+    Node merge(Node left, Node right) {
+        Node res = new Node(k);
+
+        // Product of the complete segment
+        res.prod = (left.prod * right.prod) % k;
+
+        // Prefixes that end inside the left segment
+        for (int r = 0; r < k; r++) {
+            res.cnt[r] += left.cnt[r];
+        }
+
+        // Prefixes that contain the whole left segment
+        // and then some prefix of the right segment
+        for (int r = 0; r < k; r++) {
+            int newRemainder = (left.prod * r) % k;
+            res.cnt[newRemainder] += right.cnt[r];
+        }
+
+        return res;
+    }
+
+    void build(int node, int l, int r) {
+        if (l == r) {
+            int rem = nums[l] % k;
+
+            tree[node] = new Node(k);
+            tree[node].prod = rem;
+            tree[node].cnt[rem] = 1;
+
+            return;
+        }
+
+        int mid = l + (r - l) / 2;
+
+        build(node * 2, l, mid);
+        build(node * 2 + 1, mid + 1, r);
+
+        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+    void update(int node, int l, int r, int index, int value) {
+        if (l == r) {
+            int rem = value % k;
+
+            tree[node] = new Node(k);
+            tree[node].prod = rem;
+            tree[node].cnt[rem] = 1;
+
+            return;
+        }
+
+        int mid = l + (r - l) / 2;
+
+        if (index <= mid) {
+            update(node * 2, l, mid, index, value);
+        } else {
+            update(node * 2 + 1, mid + 1, r, index, value);
+        }
+
+        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+    Node query(int node, int l, int r, int ql, int qr) {
+        // Completely outside
+        if (qr < l || r < ql) {
+            return null;
+        }
+
+        // Completely inside
+        if (ql <= l && r <= qr) {
+            return tree[node];
+        }
+
+        int mid = l + (r - l) / 2;
+
+        Node left = query(node * 2, l, mid, ql, qr);
+        Node right = query(node * 2 + 1, mid + 1, r, ql, qr);
+
+        if (left == null) return right;
+        if (right == null) return left;
+
+        return merge(left, right);
+    }
+
+    public int[] resultArray(int[] nums, int k, int[][] queries) {
+        this.nums = nums;
+        this.n = nums.length;
+        this.k = k;
+
+        tree = new Node[4 * n];
+
+        build(1, 0, n - 1);
+
+        int[] ans = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            int index = queries[i][0];
+            int value = queries[i][1];
+            int start = queries[i][2];
+            int x = queries[i][3];
+
+            // Persistent update
+            nums[index] = value;
+            update(1, 0, n - 1, index, value);
+
+            // Query nums[start ... n-1]
+            Node res = query(1, 0, n - 1, start, n - 1);
+
+            ans[i] = res.cnt[x];
+        }
+
+        return ans;
+    }
+}
